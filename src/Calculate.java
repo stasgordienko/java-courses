1ef2e870894b5042266605ca37835e7f16ad1276
stasgordienko <stang@ukr.net>
2016-04-12 11:37:06 +0300

    add Exception

--- src/Calculate.java
+++ src/Calculate.java
@@ -7,8 +7,20 @@ import static java.lang.Integer.*;
 public class Calculate {
     public static void main(String[] args){
         System.out.println("Calculating...");
-        int first = valueOf(args[0]);
-        int second = valueOf(args[1]);
+        int first, second;
+
+        try {
+            first = valueOf(args[0]);
+            second = valueOf(args[1]);
+        }
+        catch (Exception e){
+            first = 0;
+            second = 0;
+        }
+        finally {
+            //TODO: ...
+        }
+
         int sum = first + second;
         System.out.println(sum);
     }
